d3030ee5319dc48058fede72c2f9cfa512147117
ksCaesar <x246libra@hotmail.com>
2025-06-25 17:58:10 +0800

    feat: homework1 task5

--- bootcamp/materials/1-dimensional-data-modeling/homework/task4.sql
+++ bootcamp/materials/1-dimensional-data-modeling/homework/task4.sql
@@ -10,7 +10,7 @@ WITH with_prev AS (
     LAG(quality_class, 1) OVER (PARTITION BY actorid ORDER BY current_year) AS prev_quality,
     LAG(is_active, 1) OVER (PARTITION BY actorid ORDER BY current_year) AS prev_active,
     MAX(current_year) OVER () AS snapshot_year
-  FROM actors where current_year <= 1972
+  FROM actors where current_year <= 1974
 ),
 with_change AS (
   SELECT *,
@@ -58,26 +58,46 @@ DO UPDATE SET
   end_date = EXCLUDED.end_date,
   snapshot_date = EXCLUDED.snapshot_date;
 
+-- Perform a full SCD update for records where current_year <= 1974.
+--
 -- SELECT * FROM agg_data;
 -- actorid  |start_date|end_date|snapshot_date|quality_class|is_active|
 -- ---------+----------+--------+-------------+-------------+---------+
--- nm0000003|      1970|    1971|         1972|bad          |true     |
--- nm0000003|      1972|    9999|         1972|bad          |false    |
--- nm0000005|      1972|    9999|         1972|star         |true     |
--- nm0000006|      1970|    1970|         1972|average      |true     |
--- nm0000006|      1971|    9999|         1972|average      |false    |
--- nm0000008|      1971|    1971|         1972|bad          |true     |
--- nm0000008|      1972|    9999|         1972|star         |true     |
-
+-- nm0000001|      1974|    9999|         1974|average      |true     |
+-- nm0000002|      1974|    9999|         1974|good         |true     |
+-- nm0000003|      1970|    1971|         1974|bad          |true     |
+-- nm0000003|      1972|    1972|         1974|bad          |false    |
+-- nm0000003|      1973|    1973|         1974|bad          |true     |
+-- nm0000003|      1974|    9999|         1974|bad          |false    |
+-- nm0000005|      1972|    1972|         1974|star         |true     |
+-- nm0000005|      1973|    9999|         1974|star         |false    |
+-- nm0000006|      1970|    1970|         1974|average      |true     |
+-- nm0000006|      1971|    1972|         1974|average      |false    |
+-- nm0000006|      1973|    1973|         1974|average      |true     |
+-- nm0000006|      1974|    9999|         1974|good         |true     |
+-- nm0000008|      1971|    1971|         1974|bad          |true     |
+-- nm0000008|      1972|    1972|         1974|star         |true     |
+-- nm0000008|      1973|    9999|         1974|star         |false    |
+--
 -- SELECT * FROM with_segment;
 -- actorid  |current_year|quality_class|is_active|prev_quality|prev_active|snapshot_year|is_change|segment_id|
 -- ---------+------------+-------------+---------+------------+-----------+-------------+---------+----------+
--- nm0000003|        1970|bad          |true     |            |           |         1972|        1|         1|
--- nm0000003|        1971|bad          |true     |bad         |true       |         1972|        0|         1|
--- nm0000003|        1972|bad          |false    |bad         |true       |         1972|        1|         2|
--- nm0000005|        1972|star         |true     |            |           |         1972|        1|         1|
--- nm0000006|        1970|average      |true     |            |           |         1972|        1|         1|
--- nm0000006|        1971|average      |false    |average     |true       |         1972|        1|         2|
--- nm0000006|        1972|average      |false    |average     |false      |         1972|        0|         2|
--- nm0000008|        1971|bad          |true     |            |           |         1972|        1|         1|
--- nm0000008|        1972|star         |true     |bad         |true       |         1972|        1|         2|
+-- nm0000001|        1974|average      |true     |            |           |         1974|        1|         1|
+-- nm0000002|        1974|good         |true     |            |           |         1974|        1|         1|
+-- nm0000003|        1970|bad          |true     |            |           |         1974|        1|         1|
+-- nm0000003|        1971|bad          |true     |bad         |true       |         1974|        0|         1|
+-- nm0000003|        1972|bad          |false    |bad         |true       |         1974|        1|         2|
+-- nm0000003|        1973|bad          |true     |bad         |false      |         1974|        1|         3|
+-- nm0000003|        1974|bad          |false    |bad         |true       |         1974|        1|         4|
+-- nm0000005|        1972|star         |true     |            |           |         1974|        1|         1|
+-- nm0000005|        1973|star         |false    |star        |true       |         1974|        1|         2|
+-- nm0000005|        1974|star         |false    |star        |false      |         1974|        0|         2|
+-- nm0000006|        1970|average      |true     |            |           |         1974|        1|         1|
+-- nm0000006|        1971|average      |false    |average     |true       |         1974|        1|         2|
+-- nm0000006|        1972|average      |false    |average     |false      |         1974|        0|         2|
+-- nm0000006|        1973|average      |true     |average     |false      |         1974|        1|         3|
+-- nm0000006|        1974|good         |true     |average     |true       |         1974|        1|         4|
+-- nm0000008|        1971|bad          |true     |            |           |         1974|        1|         1|
+-- nm0000008|        1972|star         |true     |bad         |true       |         1974|        1|         2|
+-- nm0000008|        1973|star         |false    |star        |true       |         1974|        1|         3|
+-- nm0000008|        1974|star         |false    |star        |false      |         1974|        0|         3|
--- bootcamp/materials/1-dimensional-data-modeling/homework/task5.sql
+++ bootcamp/materials/1-dimensional-data-modeling/homework/task5.sql
@@ -1,29 +1,23 @@
 -- Incremental Update
 
 WITH
-last_scd AS ( -- Get the most recent SCD segments
+last_scd AS ( -- Get the most recent SCD records
   SELECT *
   FROM actors_history_scd
-  WHERE end_date = 1971
-),
-historical_scd AS ( -- Get historical SCD records
-  select
-  'historical' as kind,
-   *
-  FROM actors_history_scd
-  WHERE end_date < 1971
+  WHERE snapshot_date = 1973 AND end_date = 9999
 ),
 this_year AS ( -- Get current year actors data
   SELECT *
   FROM actors
-  WHERE current_year = 1972
+  WHERE current_year = 1974
 ),
-unchanged_records AS ( -- Records where status did not change, just update the end_date
+unchanged_records AS ( -- Records where status did not change, just update the end_date and snapshot_date
   select
     'unchanged' as kind,
     t.actorid,
     l.start_date,
-    t.current_year AS end_date,
+    9999 AS end_date,
+    t.current_year AS snapshot_date,
     t.quality_class,
     t.is_active
   FROM this_year t
@@ -32,62 +26,126 @@ unchanged_records AS ( -- Records where status did not change, just update the e
   WHERE t.quality_class = l.quality_class
     AND t.is_active = l.is_active
 ),
-changed_records AS ( -- Records where status changed, split into old and new segments
+changed_records AS ( -- Records where status changed, split into before and after records
   select
     t.actorid,
-    l.quality_class AS old_quality_class,
-    l.is_active AS old_is_active,
-    l.start_date AS old_start_date,
-    (t.current_year - 1)  AS old_end_date,
-    t.quality_class AS new_quality_class,
-    t.is_active AS new_is_active,
-    t.current_year AS new_start_date,
-    t.current_year AS new_end_date
+    t.current_year AS snapshot_date,
+    -- before
+    l.start_date AS before_start_date,
+    (t.current_year - 1)  AS before_end_date,
+    l.quality_class AS before_quality_class,
+    l.is_active AS before_is_active,
+    -- after
+    t.current_year AS after_start_date,
+    9999 AS after_end_date,
+    t.quality_class AS after_quality_class,
+    t.is_active AS after_is_active
   FROM this_year t
   JOIN last_scd l
     ON t.actorid = l.actorid
   WHERE t.quality_class <> l.quality_class
      OR t.is_active <> l.is_active
 ),
-insert_records AS ( -- Records for actors who did not appear in last year's SCD
+split_changed_records AS ( -- Split changed records into before records and after records
   select
-    'insert_records' as kind,
+    'before_changed' as kind,
+    actorid,
+    before_start_date AS start_date,
+    before_end_date AS end_date,
+    snapshot_date,
+    before_quality_class AS quality_class,
+    before_is_active AS is_active
+  FROM changed_records
+    UNION ALL
+  select
+    'after_changed' as kind,
+    actorid,
+    after_start_date AS start_date,
+    after_end_date AS end_date,
+    snapshot_date,
+    after_quality_class AS quality_class,
+    after_is_active AS is_active
+  FROM changed_records
+),
+new_records AS ( -- Records for actors who did not appear in last year's SCD
+  select
+    'new_records' as kind,
     t.actorid,
     t.current_year AS start_date,
-    t.current_year AS end_date,
-    t.quality_class, 
+    9999 AS end_date,
+    t.current_year AS snapshot_date,
+    t.quality_class,
     t.is_active
   FROM this_year t
   LEFT JOIN last_scd l
     ON t.actorid = l.actorid
   WHERE l.actorid IS NULL
 ),
-split_changed_records AS ( -- Split changed records into old segment and new segment
+historical_scd AS ( -- Get historical SCD records
   select
-    'old_changed' as kind,
-    actorid,
-    old_start_date AS start_date,
-    old_end_date AS end_date,
-    old_quality_class AS quality_class,
-    old_is_active AS is_active
-  FROM changed_records
-    UNION ALL
-  select
-    'new_changed' as kind,
-    actorid,
-    new_start_date AS start_date,
-    new_end_date AS end_date,
-    new_quality_class AS quality_class,
-    new_is_active AS is_active
-  FROM changed_records
+  'historical' as kind,
+   *
+  FROM actors_history_scd
+  WHERE end_date < 9999
 ),
-combined_data AS ( -- Combine all segments into one result set
-  SELECT * FROM historical_scd
-    UNION ALL
+combined_data AS ( -- Combine all records into one result set
   SELECT * FROM unchanged_records
     UNION ALL
   SELECT * FROM split_changed_records
     UNION ALL
-  SELECT * FROM insert_records
+  SELECT * FROM new_records
+    UNION ALL
+  SELECT * FROM historical_scd
 )
-SELECT * FROM combined_data ORDER BY actorid, start_date;
+INSERT INTO actors_history_scd
+--SELECT * FROM combined_data ORDER BY actorid, start_date
+SELECT actorid, start_date, end_date, snapshot_date, quality_class, is_active FROM combined_data ORDER BY actorid, start_date
+ON CONFLICT (actorid, start_date)
+DO UPDATE SET
+  quality_class = EXCLUDED.quality_class,
+  is_active = EXCLUDED.is_active,
+  end_date = EXCLUDED.end_date,
+  snapshot_date = EXCLUDED.snapshot_date;
+
+-- Perform a full SCD update for records where current_year <= 1973,
+-- then perform an incremental SCD update using this_year = 1974.
+--
+-- SELECT * FROM combined_data ORDER BY actorid, start_date;
+-- kind          |actorid  |start_date|end_date|snapshot_date|quality_class|is_active|
+-- --------------+---------+----------+--------+-------------+-------------+---------+
+-- new_records   |nm0000001|      1974|    9999|         1974|average      |true     |
+-- new_records   |nm0000002|      1974|    9999|         1974|good         |true     |
+-- historical    |nm0000003|      1970|    1971|         1973|bad          |true     |
+-- historical    |nm0000003|      1972|    1972|         1973|bad          |false    |
+-- before_changed|nm0000003|      1973|    1973|         1974|bad          |true     |
+-- after_changed |nm0000003|      1974|    9999|         1974|bad          |false    |
+-- historical    |nm0000005|      1972|    1972|         1973|star         |true     |
+-- unchanged     |nm0000005|      1973|    9999|         1974|star         |false    |
+-- historical    |nm0000006|      1970|    1970|         1973|average      |true     |
+-- historical    |nm0000006|      1971|    1972|         1973|average      |false    |
+-- before_changed|nm0000006|      1973|    1973|         1974|average      |true     |
+-- after_changed |nm0000006|      1974|    9999|         1974|good         |true     |
+-- historical    |nm0000008|      1971|    1971|         1973|bad          |true     |
+-- historical    |nm0000008|      1972|    1972|         1973|star         |true     |
+-- unchanged     |nm0000008|      1973|    9999|         1974|star         |false    |
+
+-- Perform multiple incremental SCD updates, one per year from current_year = 1970 to 1974.
+--
+-- SELECT * FROM combined_data ORDER BY actorid, start_date;
+-- kind          |actorid  |start_date|end_date|snapshot_date|quality_class|is_active|
+-- --------------+---------+----------+--------+-------------+-------------+---------+
+-- new_records   |nm0000001|      1974|    9999|         1974|average      |true     |
+-- new_records   |nm0000002|      1974|    9999|         1974|good         |true     |
+-- historical    |nm0000003|      1970|    1971|         1972|bad          |true     |
+-- historical    |nm0000003|      1972|    1972|         1973|bad          |false    |
+-- before_changed|nm0000003|      1973|    1973|         1974|bad          |true     |
+-- after_changed |nm0000003|      1974|    9999|         1974|bad          |false    |
+-- historical    |nm0000005|      1972|    1972|         1973|star         |true     |
+-- unchanged     |nm0000005|      1973|    9999|         1974|star         |false    |
+-- historical    |nm0000006|      1970|    1970|         1971|average      |true     |
+-- historical    |nm0000006|      1971|    1972|         1973|average      |false    |
+-- before_changed|nm0000006|      1973|    1973|         1974|average      |true     |
+-- after_changed |nm0000006|      1974|    9999|         1974|good         |true     |
+-- historical    |nm0000008|      1971|    1971|         1972|bad          |true     |
+-- historical    |nm0000008|      1972|    1972|         1973|star         |true     |
+-- unchanged     |nm0000008|      1973|    9999|         1974|star         |false    |
