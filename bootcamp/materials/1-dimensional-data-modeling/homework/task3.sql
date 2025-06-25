0f344d9ed696a89a74fcf67d73277da1e7790184
ksCaesar <x246libra@hotmail.com>
2025-06-25 16:07:52 +0800

    refactor: add snapshot_date

--- bootcamp/materials/1-dimensional-data-modeling/homework/task3.sql
+++ bootcamp/materials/1-dimensional-data-modeling/homework/task3.sql
@@ -1,13 +1,35 @@
 -- DROP TABLE IF EXISTS actors_history_scd;
 
 CREATE TABLE actors_history_scd (
-  actorid       TEXT NOT NULL,
-  start_date    INTEGER NOT NULL,
-  end_date      INTEGER NOT NULL,
-  quality_class quality_class,
-  is_active     BOOLEAN,
-  PRIMARY KEY (actorid, start_date, end_date)
+  actorid        TEXT NOT NULL,
+  start_date     INTEGER NOT NULL,
+  end_date       INTEGER NOT NULL,
+  snapshot_date  INTEGER NOT NULL,
+  quality_class  quality_class,
+  is_active      BOOLEAN,
+  PRIMARY KEY (actorid, start_date),
+  CHECK (end_date >= start_date)
 );
 
 -- https://github.com/copilot/share/4a3e5096-4ba0-8426-9813-880860df40b8
 -- https://discord.com/channels/1106357930443407391/1386647289002594314
+
+-- start_date
+-- Whose time? Business data time
+-- Description:
+-- When this state started being valid
+
+-- end_date
+-- Whose time? Business data time
+-- Description:
+-- When this state stopped being valid
+
+-- snapshot_date
+-- Whose time? Source data time
+-- Description:
+-- This ETL process extracts business data from a specific point in time in the source system, capturing the whole window of the data.
+
+-- process_date
+-- Whose time? ETL system time
+-- Description:
+-- When this row was processed and written into the table
--- bootcamp/materials/1-dimensional-data-modeling/homework/task4.sql
+++ bootcamp/materials/1-dimensional-data-modeling/homework/task4.sql
@@ -8,8 +8,9 @@ WITH with_prev AS (
     quality_class,
     is_active,
     LAG(quality_class, 1) OVER (PARTITION BY actorid ORDER BY current_year) AS prev_quality,
-    LAG(is_active, 1) OVER (PARTITION BY actorid ORDER BY current_year) AS prev_active
-  FROM actors
+    LAG(is_active, 1) OVER (PARTITION BY actorid ORDER BY current_year) AS prev_active,
+    MAX(current_year) OVER () AS snapshot_year
+  FROM actors where current_year <= 1972
 ),
 with_change AS (
   SELECT *,
@@ -23,6 +24,8 @@ with_change AS (
     END AS is_change
   FROM with_prev
 ),
+-- "streak" is commonly used to describe the number of consecutive days or occurrences of an event or state.
+-- Here, we use it to identify segments of consecutive years with the same quality_class and is_active status.
 with_segment AS (
   SELECT *,
     SUM(is_change) OVER (PARTITION BY actorid ORDER BY current_year) AS segment_id
@@ -32,8 +35,13 @@ agg_data AS (
   SELECT
     actorid,
     MIN(current_year) AS start_date,
-    MAX(current_year) AS end_date,
-    -- Use ANY_VALUE or MIN/MAX on the text representation
+    -- set end_date to 9999 if it's the latest segment
+    CASE
+      WHEN ROW_NUMBER() OVER (PARTITION BY actorid ORDER BY segment_id DESC) = 1 THEN 9999
+      ELSE MAX(current_year)
+    END AS end_date,
+    -- The following fields use MIN() only to satisfy SQL aggregation syntax (all values in segment should be same)
+    MIN(snapshot_year) AS snapshot_date,
     (MIN(quality_class::TEXT))::quality_class AS quality_class,
     -- Use BOOL_AND for is_active (all values in segment should be same)
     BOOL_AND(is_active) AS is_active
@@ -43,31 +51,33 @@ agg_data AS (
 )
 INSERT INTO actors_history_scd
 SELECT * FROM agg_data
-ON CONFLICT (actorid, start_date, end_date)
+ON CONFLICT (actorid, start_date)
 DO UPDATE SET
   quality_class = EXCLUDED.quality_class,
-  is_active = EXCLUDED.is_active;
+  is_active = EXCLUDED.is_active,
+  end_date = EXCLUDED.end_date,
+  snapshot_date = EXCLUDED.snapshot_date;
 
 -- SELECT * FROM agg_data;
--- actorid  |start_date|end_date|quality_class|is_active|
--- ---------+----------+--------+-------------+---------+
--- nm0000003|      1970|    1971|bad          |true     |
--- nm0000003|      1972|    1972|bad          |false    |
--- nm0000005|      1972|    1972|star         |true     |
--- nm0000006|      1970|    1970|average      |true     |
--- nm0000006|      1971|    1972|average      |false    |
--- nm0000008|      1971|    1971|bad          |true     |
--- nm0000008|      1972|    1972|star         |true     |
---
+-- actorid  |start_date|end_date|snapshot_date|quality_class|is_active|
+-- ---------+----------+--------+-------------+-------------+---------+
+-- nm0000003|      1970|    1971|         1972|bad          |true     |
+-- nm0000003|      1972|    9999|         1972|bad          |false    |
+-- nm0000005|      1972|    9999|         1972|star         |true     |
+-- nm0000006|      1970|    1970|         1972|average      |true     |
+-- nm0000006|      1971|    9999|         1972|average      |false    |
+-- nm0000008|      1971|    1971|         1972|bad          |true     |
+-- nm0000008|      1972|    9999|         1972|star         |true     |
+
 -- SELECT * FROM with_segment;
--- actorid  |current_year|quality_class|is_active|prev_quality|prev_active|is_change|segment_id|
--- ---------+------------+-------------+---------+------------+-----------+---------+----------+
--- nm0000003|        1970|bad          |true     |            |           |        1|         1|
--- nm0000003|        1971|bad          |true     |bad         |true       |        0|         1|
--- nm0000003|        1972|bad          |false    |bad         |true       |        1|         2|
--- nm0000005|        1972|star         |true     |            |           |        1|         1|
--- nm0000006|        1970|average      |true     |            |           |        1|         1|
--- nm0000006|        1971|average      |false    |average     |true       |        1|         2|
--- nm0000006|        1972|average      |false    |average     |false      |        0|         2|
--- nm0000008|        1971|bad          |true     |            |           |        1|         1|
--- nm0000008|        1972|star         |true     |bad         |true       |        1|         2|
+-- actorid  |current_year|quality_class|is_active|prev_quality|prev_active|snapshot_year|is_change|segment_id|
+-- ---------+------------+-------------+---------+------------+-----------+-------------+---------+----------+
+-- nm0000003|        1970|bad          |true     |            |           |         1972|        1|         1|
+-- nm0000003|        1971|bad          |true     |bad         |true       |         1972|        0|         1|
+-- nm0000003|        1972|bad          |false    |bad         |true       |         1972|        1|         2|
+-- nm0000005|        1972|star         |true     |            |           |         1972|        1|         1|
+-- nm0000006|        1970|average      |true     |            |           |         1972|        1|         1|
+-- nm0000006|        1971|average      |false    |average     |true       |         1972|        1|         2|
+-- nm0000006|        1972|average      |false    |average     |false      |         1972|        0|         2|
+-- nm0000008|        1971|bad          |true     |            |           |         1972|        1|         1|
+-- nm0000008|        1972|star         |true     |bad         |true       |         1972|        1|         2|
